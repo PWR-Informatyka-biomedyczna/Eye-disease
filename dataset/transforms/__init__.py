f726ab998a1a92f07fcb2889be52b30b5c94ae44
Adam Chlopowiec <yeabunnypl@gmail.com>
2021-12-02 09:49:13 +0100

    Fixed some things

--- dataset/transforms/__init__.py
+++ dataset/transforms/__init__.py
@@ -10,10 +10,27 @@ import cv2
 def train_transforms(
     target_size: Tuple[int, int],
     normalize: bool = True, 
-    interpolation_mode: InterpolationMode = InterpolationMode.NEAREST) -> transforms.Compose:
+    interpolation_mode = cv2.INTER_NEAREST) -> transforms.Compose:
+    aug_A = A.Compose(
+                    [
+                        A.Resize(target_size[0], target_size[1], interpolation=interpolation_mode),
+                        A.Rotate(limit=(-5, 5), p=0.5, interpolation=interpolation_mode),
+                        A.HorizontalFlip(p=0.5),
+                        A.VerticalFlip(p=0.5),
+                        A.GaussianBlur(p=0.3),
+                        A.Equalize(by_channels=False, p=0.3)
+                    ])
+    aug_ia = iaa.Sometimes(iaa.OneOf([
+                iaa.AdditiveGaussianNoise(),
+                iaa.LinearContrast(),
+                iaa.AddToBrightness()
+             ]))
+
     transforms_list = [
-            transforms.ToTensor()
-        ]
+        aug_A,
+        aug_ia,
+        transforms.ToTensor()
+    ]
     if normalize:
         transforms_list.append(transforms.Normalize(
             (0.485, 0.456, 0.406),
@@ -25,7 +42,9 @@ def test_val_transforms(
     target_size: Tuple[int, int], 
     normalize: bool = True,
     interpolation_mode: InterpolationMode = InterpolationMode.NEAREST) -> transforms.Compose:
+
     transforms_list = [
+        transforms.Resize(target_size, interpolation=interpolation_mode),
         transforms.ToTensor()
     ]
     if normalize:
@@ -34,24 +53,3 @@ def test_val_transforms(
             (0.229, 0.224, 0.225)
         ))
     return transforms.Compose(transforms_list)
-
-def extended_train_transforms(
-        target_size: Tuple[int, int], 
-        interpolation_mode = cv2.INTER_NEAREST) -> transforms.Compose:
-    
-    aug_A = A.Compose(
-                    [
-                        A.Resize(target_size[0], target_size[1]),
-                        A.Rotate(limit=(-5, 5), p=0.5, interpolation=interpolation_mode),
-                        A.HorizontalFlip(p=0.5),
-                        A.VerticalFlip(p=0.5),
-                        A.GaussianBlur(blur_limit=(5, 5), p=0.3),
-                        A.Equalize(by_channels=False, p=0.2)
-                    ])
-    aug_ia = iaa.OneOf([
-                iaa.AdditiveGaussianNoise(),
-                iaa.LinearContrast(),
-                iaa.AddToBrightness()
-             ])
-    # Tune parametry tych augmentacji
-    return aug_A, aug_ia
