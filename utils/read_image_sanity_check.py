35782591641bfb7eeeaeb9a55391014d90937c4a
konrad-karanowski <konrad.karanowski@gmail.com>
2021-12-11 21:02:58 +0100

    Finished resizing and testing resized data

--- utils/read_image_sanity_check.py
+++ utils/read_image_sanity_check.py
@@ -7,7 +7,8 @@ from PIL import Image
 
 
 PATH_COLUMN_NAME = 'Path'
-CSV_PATH = '/media/data/adam_chlopowiec/eye_image_classification/collected_data_splits.csv'
+# Changed to resized dataset csv
+CSV_PATH = '/media/data/adam_chlopowiec/eye_image_classification/resized_collected_data.csv'
 
 
 class Counter:
@@ -53,9 +54,9 @@ def sanity_check_cv2(df: pd.DataFrame):
 def main():
     df = pd.read_csv(CSV_PATH)
     pilcounter = sanity_check_pil(df)
-    cvcounter = sanity_check_cv2(df)
+    #cvcounter = sanity_check_cv2(df)
     print(f'PIL: {pilcounter}')
-    print(f'CV2: {cvcounter}')
+    #print(f'CV2: {cvcounter}')
     print(f'Total data: {len(df)}')
 
 
