a0ee1dc40a2ff1de10bd95bcd7fcb03dc92e1424
Adam-Chlopowiec <adam.chlopowiec@gmail.com>
2021-12-30 23:00:39 +0100

    Updated hiperparams of transforms and updated sgd and adamw sweeps

--- experiments/base_experiment.py
+++ experiments/base_experiment.py
@@ -25,55 +25,23 @@ from training import train_test
 # PL_TORCH_DISTRIBUTED_BACKEND=gloo poetry run python3 -m experiments.base_experiment
 # experiment setup
 SEED = 0
-PROJECT_NAME = 'ResNetTraining'
+PROJECT_NAME = 'ResNet18Optimizing'
 NUM_CLASSES = 4
-LR = 1e-4
-BATCH_SIZE = 64
 MAX_EPOCHS = 100
 NORMALIZE = True
 MONITOR = 'val_loss'
-PATIENCE = 5
+PATIENCE = 10
 GPUS = -1
 ENTITY_NAME = 'kn-bmi'
 RESAMPLER = resamplers.identity_resampler
-WEIGHTS = torch.Tensor([1, 2, 2.5, 1.5])
-TYPE = 'test' # pretraining, training, training-from-pretrained
-MODEL_PATH = '/home/adam_chlopowiec/data/eye_image_classification/Eye-disease/checkpoints/training/ResNet18Model/2021-12-16_16:22:11.605147/ResNet18Model.ckpt'
-TEST_ONLY = True
+TYPE = 'training-from-pretrained' # pretraining, training, training-from-pretrained
+MODEL_PATH = '/home/adam_chlopowiec/data/eye_image_classification/Eye-disease/checkpoints/pretraining/ResNet18Model/2021-12-15_15:59:16.045619/ResNet18Model.ckpt'
+TEST_ONLY = False
 DATE_NOW = str(datetime.datetime.now())
 
 models_list = [
-        #EfficientNetB0(NUM_CLASSES),
-        #EfficientNetB2(NUM_CLASSES),
-        #Xception(NUM_CLASSES),
-        #DenseNet(NUM_CLASSES),
-        #ResNext50(NUM_CLASSES),
-        ResNet18Model(NUM_CLASSES),
-        #ResNet50Model(NUM_CLASSES)
+        ResNet18Model(NUM_CLASSES)
     ]
-
-
-def load_model(model, mode: str = 'train'):
-    classifier = Classifier(
-                model=model,
-                num_classes=NUM_CLASSES,
-                lr=LR,
-                optimizer=torch.optim.Adam,
-                weights=WEIGHTS
-                )
-    in_features = model.get_last_layer().in_features
-    out_features = model.get_last_layer().out_features
-    if mode == 'train':
-        if out_features > 2:
-            model.set_last_layer(torch.nn.Linear(in_features, 2))
-        classifier.load_from_checkpoint(checkpoint_path=MODEL_PATH, model=model, num_classes=NUM_CLASSES, lr=LR, weights=WEIGHTS[:2])
-        classifier.model.set_last_layer(torch.nn.Linear(in_features, NUM_CLASSES))
-        classifier.criterion.weight = WEIGHTS
-    elif mode == 'test':
-        if out_features < NUM_CLASSES:
-            model.set_last_layer(torch.nn.Linear(in_features, NUM_CLASSES))
-        classifier.load_from_checkpoint(checkpoint_path=MODEL_PATH, model=model, num_classes=NUM_CLASSES, lr=LR, weights=WEIGHTS)
-    return classifier
 
 
 def seed_all(seed: int) -> None:
@@ -81,30 +49,174 @@ def seed_all(seed: int) -> None:
     torch.random.manual_seed(seed)
     random.seed(seed)
 
+def load_model(model, optimizer=None, lr_scheduler=None, mode: str = 'train', lr=1e-4, weights=torch.Tensor([1, 1, 2.5, 2])):
+    classifier = Classifier(
+                model=model,
+                num_classes=NUM_CLASSES,
+                lr=lr,
+                weights=torch.Tensor([1, 1]),
+                optimizer=None,
+                lr_scheduler=None
+                )
+    in_features = model.get_last_layer().in_features
+    out_features = model.get_last_layer().out_features
+    if mode == 'train':
+        if out_features > 2:
+            model.set_last_layer(torch.nn.Linear(in_features, 2))
+        classifier.load_from_checkpoint(checkpoint_path=MODEL_PATH, model=model, 
+                                        num_classes=NUM_CLASSES, lr=lr, weights=torch.Tensor([1, 1]), optimizer=None, lr_scheduler=None)
+        classifier.model.set_last_layer(torch.nn.Linear(in_features, NUM_CLASSES))
+    elif mode == 'test':
+        if out_features < NUM_CLASSES:
+            model.set_last_layer(torch.nn.Linear(in_features, NUM_CLASSES))
+        classifier.load_from_checkpoint(checkpoint_path=MODEL_PATH, model=model, 
+                                        num_classes=NUM_CLASSES, lr=lr, weights=torch.Tensor(1, 1, 1, 1), 
+                                        optimizer=None, lr_scheduler=None)
+    classifier.optimizer = optimizer
+    classifier.lr_scheduler = lr_scheduler
+    classifier.criterion.weight = weights
+    
+    return classifier
+
+
+def init_const_values(config):
+    lr = config.learning_rate
+    batch_size = config.batch_size
+    weight_0 = config.weight_0
+    weight_1 = config.weight_1
+    weight_2 = config.weight_2
+    weight_3 = config.weight_3
+    weights = torch.Tensor([weight_0, weight_1, weight_2, weight_3])
+    return lr, batch_size, weights
+
+
+def init_optimizer(model, config, lr=1e-4):
+    classifier = Classifier(
+                model=model,
+                num_classes=NUM_CLASSES,
+                lr=lr,
+                weights=torch.Tensor([1, 1]),
+                optimizer=None,
+                lr_scheduler=None
+                )
+    in_features = model.get_last_layer().in_features
+    out_features = model.get_last_layer().out_features
+    if out_features > 2:
+            model.set_last_layer(torch.nn.Linear(in_features, 2))
+    dummy_classifier = classifier.load_from_checkpoint(checkpoint_path=MODEL_PATH, model=model, 
+                                        num_classes=NUM_CLASSES, lr=1e-4, weights=torch.Tensor([1, 1]), optimizer=None, lr_scheduler=None)
+    dummy_classifier.model.set_last_layer(torch.nn.Linear(in_features, NUM_CLASSES))
+    model = dummy_classifier.model
+    if config is not None:
+        optimizer = config.optimizer
+        if optimizer == 'adam':
+            beta = config.beta
+            weight_decay = config.weight_decay
+            amsgrad = config.amsgrad
+            return torch.optim.Adam(model.parameters(), lr=lr, betas=(0.9, beta), weight_decay=weight_decay, amsgrad=amsgrad)
+        
+        elif optimizer == 'adamw':
+            beta = config.beta
+            weight_decay = config.weight_decay
+            amsgrad = config.amsgrad    
+            return torch.optim.AdamW(model.parameters(), lr=lr, betas=(0.9, beta), weight_decay=weight_decay, amsgrad=amsgrad)
+        
+        elif optimizer == 'adamax':
+            beta = config.beta
+            weight_decay = config.weight_decay
+            return torch.optim.Adamax(model.parameters(), lr=lr, betas=(0.9, beta), weight_decay=weight_decay)
+        
+        elif optimizer == 'radam':
+            beta = config.beta
+            weight_decay = config.weight_decay
+            return torch.optim.RAdam(model.parameters(), lr=lr, betas=(0.9, beta), weight_decay=weight_decay)
+        
+        elif optimizer == 'nadam':
+            beta = config.beta
+            momentum_decay = config.momentum_decay
+            weight_decay = config.weight_decay
+            return torch.optim.NAdam(model.parameters(), lr=lr, betas=(0.9, beta), weight_decay=weight_decay, momentum_decay=momentum_decay)
+        
+        elif optimizer == 'rmsprop':
+            weight_decay = config.weight_decay
+            alpha = config.alpha
+            momentum = config.momentum 
+            centered = config.centered
+            return torch.optim.RMSprop(model.parameters(), lr=lr, alpha=alpha, momentum=momentum, weight_decay=weight_decay, centered=centered)
+        
+        elif optimizer == 'sgd':
+            weight_decay = config.weight_decay
+            dampening = config.dampening
+            nesterov = config.nesterov
+            momentum = config.momentum
+            if nesterov:
+                return torch.optim.SGD(model.parameters(), lr=lr, weight_decay=weight_decay,
+                                momentum=momentum, dampening=0, nesterov=nesterov)
+            else:
+                return torch.optim.SGD(model.parameters(), lr=lr, weight_decay=weight_decay,
+                                momentum=momentum, dampening=dampening, nesterov=nesterov)
+        
+        elif optimizer == 'asgd':
+            alpha = config.alpha_asgd
+            lambda_ = config.lambda_asgd
+            t0 = config.t0_asgd
+            weight_decay = config.weight_decay
+            return torch.optim.ASGD(model.parameters(), lr=lr, lambd=lambda_, alpha=alpha, t0=t0, weight_decay=weight_decay)
+    else:
+        return torch.optim.SGD(model.parameters(), lr=lr, weight_decay=1e-7, momentum=4e-4, dampening=0, nesterov=False)
+    return None
+
+
+def init_scheduler(optimizer, config):
+    scheduler_params = config.lr_scheduler
+    scheduler_params = scheduler_params.split('_')
+    if scheduler_params[0] == 'multiplicativelr':
+        lr_lambda = lambda epoch: float(scheduler_params[1])
+        return torch.optim.lr_scheduler.MultiplicativeLR(optimizer=optimizer, lr_lambda=lr_lambda)
+    elif scheduler_params[0] == 'cosinelr':
+        t_max = int(scheduler_params[1])
+        return torch.optim.lr_scheduler.CosineAnnealingLR(optimizer=optimizer, T_max=t_max)
+    elif scheduler_params[0] == 'cosinewarmlr':
+        t_0 = int(scheduler_params[1])
+        t_mul = int(scheduler_params[2])
+        return torch.optim.lr_scheduler.CosineAnnealingWarmRestarts(optimizer=optimizer, T_0=t_0, T_mult=t_mul)
+    elif scheduler_params[0] == 'None':
+        return None
+    
+    return None
+
+
+def create_log_path(model):
+    mode = str(TYPE)
+    if isinstance(model, Classifier):
+        model_type = type(model.model).__name__
+        input_size = model.model.input_size
+        run_save_dir = mode + '/' + type(model.model).__name__  + '/' + str(DATE_NOW)
+    else:
+        model_type = type(model).__name__
+        input_size = model.input_size
+        run_save_dir = mode + '/' + type(model).__name__  + '/' + str(DATE_NOW)
+    
+    run_save_dir = run_save_dir.replace(" ", "_")
+    path = str(CHECKPOINTS_DIR)
+    checkpoints_run_dir = path + '/' + run_save_dir
+    return checkpoints_run_dir, model_type, input_size
+
 
 def main():
     seed_all(SEED)
+    lr, batch_size, weights = 1e-1, 64, torch.Tensor([0.9, 1.3, 2.7, 2])
+    
     for model in models_list:
-        
+        optimizer = init_optimizer(model, None, lr=lr)
+        lr_scheduler = torch.optim.lr_scheduler.CosineAnnealingWarmRestarts(optimizer=optimizer, T_0=8, T_mult=1)
         if MODEL_PATH is not None:
             if TEST_ONLY:
-                model = load_model(model, mode='test')
+                model = load_model(model, optimizer=optimizer, lr_scheduler=lr_scheduler, mode='test', lr=lr, weights=weights)
             else:
-                model = load_model(model, mode='train')
+                model = load_model(model, optimizer=optimizer, lr_scheduler=lr_scheduler, mode='train', lr=lr, weights=weights)
         
-        mode = str(TYPE)
-        if isinstance(model, Classifier):
-            model_type = type(model.model).__name__
-            input_size = model.model.input_size
-            run_save_dir = mode + '/' + type(model.model).__name__  + '/' + str(DATE_NOW)
-        else:
-            model_type = type(model).__name__
-            input_size = model.input_size
-            run_save_dir = mode + '/' + type(model).__name__  + '/' + str(DATE_NOW)
-        
-        run_save_dir = run_save_dir.replace(" ", "_")
-        path = str(CHECKPOINTS_DIR)
-        checkpoints_run_dir = path + '/' + run_save_dir
+        checkpoints_run_dir, model_type, input_size = create_log_path(model)
         Path(checkpoints_run_dir).mkdir(mode=777, parents=True, exist_ok=True)
         
         data_module = EyeDiseaseDataModule(
@@ -118,7 +230,7 @@ def main():
             image_path_name='Path',
             target_name='Label',
             split_name='Split',
-            batch_size=BATCH_SIZE,
+            batch_size=batch_size,
             num_workers=12,
             shuffle_train=True,
             resampler=RESAMPLER
@@ -128,12 +240,12 @@ def main():
         hparams = {
             'dataset': type(data_module).__name__,
             'model_type': model_type,
-            'lr': LR,
-            'batch_size': BATCH_SIZE,
-            'optimizer': 'Adam',
+            'lr': lr,
+            'batch_size': batch_size,
+            'optimizer': type(optimizer).__name__,
             'resampler': RESAMPLER.__name__,
             'num_classes': NUM_CLASSES,
-            'run_id': run_save_dir
+            #'run_id': run_save_dir
         }
 
         logger = WandbLogger(
@@ -164,10 +276,12 @@ def main():
             max_epochs=MAX_EPOCHS,
             num_classes=NUM_CLASSES,
             gpus=GPUS,
-            lr=LR,
+            lr=lr,
             callbacks=callbacks,
             logger=logger,
-            weights=WEIGHTS,
+            weights=weights,
+            optimizer=optimizer,
+            lr_scheduler=lr_scheduler,
             test_only=TEST_ONLY
         )
         logger.experiment.finish()
